e91c1c95ef489829472e5597a34b38dbe31b010f
Muhammad Salman <92182319+m-salman004@users.noreply.github.com>
2021-12-28 10:45:19 -0800

    Global Variables

---
+++ script.js
@@ -0,0 +1,5 @@
+// Golbal variables
+const input = document.getElementById("input"),
+    output = document.getElementById("output"),
+    originalString = document.getElementById("originalString");
+let cities = ["Faisalabad", "Lahore", "Islamabad", "Karachi"]
